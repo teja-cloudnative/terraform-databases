df7c5d1145583ca7acc3f25d97c655bfa971a965
tejap <btejapramod@gmail.com>
2022-03-04 12:30:46 +0530

    New Changes

--- main.tf
+++ main.tf
@@ -1,15 +1,17 @@
 module "mongodb" {
-  source = "./mongodb"
+  source                = "./mongodb"
+  ENV                   = var.ENV
+  MONGODB_INSTANCE_TYPE = var.MONGODB_INSTANCE_TYPE
 }
 
-module "mysql" {
-  source = "./mysql"
-}
-
-module "rabbitmq" {
-  source = "./rabbitmq"
-}
-
-module "redis" {
-  source = "./redis"
-}
+#module "mysql" {
+#  source = "./mysql"
+#}
+#
+#module "rabbitmq" {
+#  source = "./rabbitmq"
+#}
+#
+#module "redis" {
+#  source = "./redis"
+#}
