3583c084c43facaa56bf002c232d2b7c6fe261ea
tejap <btejapramod@gmail.com>
2022-03-04 13:16:48 +0530

    New Changes

--- mongodb/ec2.tf
+++ mongodb/ec2.tf
@@ -2,7 +2,7 @@ resource "aws_spot_instance_request" "spot-instance" {
   ami                    = data.aws_ami.ami.id
   instance_type          = var.MONGODB_INSTANCE_TYPE
   wait_for_fulfillment   = true
-  subnet_id     = element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS_ID, count.index)
+  subnet_id     = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS_ID[0]
   vpc_security_group_ids = [aws_security_group.allow-mongodb.id]
   //  root_block_device {
   //    volume_size = 10
