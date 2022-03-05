4f0a872b7dfe96206a81ef4e2d4fcd7c6cfcb183
tejap <btejapramod@gmail.com>
2022-03-05 07:41:01 +0530

    New Changes

--- dev.tfvars
+++ dev.tfvars
@@ -1,3 +1,5 @@
 ENV = "dev"
 MONGODB_INSTANCE_TYPE = "t2.micro"
 RABBITMQ_INSTANCE_TYPE = "t2.micro"
+
+#This is Rabbitmq
--- main.tf
+++ main.tf
@@ -1,22 +1,22 @@
 module "mongodb" {
-  source                = "./mongodb"
+  source                = "mongodb"
   ENV                   = var.ENV
   MONGODB_INSTANCE_TYPE = var.MONGODB_INSTANCE_TYPE
 }
 
 module "rabbitmq" {
-  source                 = "./rabbitmq"
+  source                 = "rabbitmq"
   ENV                    = var.ENV
   RABBITMQ_INSTANCE_TYPE = var.RABBITMQ_INSTANCE_TYPE
 }
 
 module "mysql" {
-  source = "./mysql"
+  source = "mysql"
   ENV    = var.ENV
 }
 
 module "redis" {
-  source = "./redis"
+  source = "redis"
   ENV    = var.ENV
 }
 
